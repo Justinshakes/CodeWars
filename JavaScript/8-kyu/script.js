bbc8aefbc8a5f515cca61288cae5a91735cc254c
Justin Sherrell <Superjustin44@gmail.com>
2024-03-17 16:36:48 -0700

    learning JS!

---
+++ JavaScript/8-kyu/script.js
@@ -0,0 +1,10 @@
+// Even or Odd
+function script(number) {
+    return (number % 2 === 0) ? "Even" : "Odd";
+}
+
+// Opposites Attract
+function lovefunc(flower1, flower2){
+    return (flower1 + flower2) % 2 === 1;
+}
+
